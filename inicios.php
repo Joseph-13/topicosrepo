f6b066aae377265093ef4797b266f2cdb1066071
Joseph Sandoval <51730237+Joseph-13@users.noreply.github.com>
2021-01-11 14:18:56 -0800

    Cambio de letrero de index versión 1.02 se queda

--- inicios.php
+++ inicios.php
@@ -27,7 +27,7 @@ body{background: url(carro.jpg) no-repeat center; background-size: 1265px 965px}
                 <a href="/" class="navbar-brand"></a>
             </nav>
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">CRUD </a>
+    <a class="navbar-brand" href="#">Autos JL </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
